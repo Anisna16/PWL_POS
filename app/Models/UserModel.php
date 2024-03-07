545f2eea2336b337c8c983cc6b59f18b6569e637
Anisna Hilwa Nadhifah <nadhifahanisna@gmail.com>
2024-03-07 13:58:30 +0700

    Fillable

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class UserModel extends Model
 {
@@ -11,4 +12,11 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function setPasswordAttribued($value)
+    {
+        $this->attributes['password']=Hash::make($value);
+    }
 }
