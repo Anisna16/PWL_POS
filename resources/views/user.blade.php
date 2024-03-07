4d156d8e3d073f6167b4c06062955580c49aba70
Anisna Hilwa Nadhifah <nadhifahanisna@gmail.com>
2024-03-07 15:06:23 +0700

    Retriving Agregates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,25 +1,14 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Data User</title>
-    </head>
+<html lang="en">
     <body>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centered Box</title>
+</head>
+<body style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <div style="width: 300px; padding: 20px; border: 1px solid #000; text-align: center;">
         <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
-            </tr>
-            @foreach ($data as $d)
-            <tr>
-                <td>{{$d->user_id}}</td>
-                <td>{{$d->username}}</td>
-                <td>{{$d->nama}}</td>
-                <td>{{$d->level_id}}</td>
-            </tr>
-            @endforeach
-        </table>
+        <p>Jumlah Pengguna</p>
+    </div>
     </body>
 </html>
