1d92631b52a83f5074a99217152c5707eb632ca7
Anisna Hilwa Nadhifah <nadhifahanisna@gmail.com>
2024-03-11 21:54:14 +0700

    Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user=UserModel::firstOrCreate(
+        $user=UserModel::firstOrNew(
             [
-                'username' => 'manager',
-                'nama' => 'Manager',
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make(12345),
+                'level_id' => 2
             ],
         );
-        
+        $user-> save();
+
         return view('user', ['data' => $user]);
     }
 }
